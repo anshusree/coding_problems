e6e864f7eda27091e3a204a7dd88e3de7f235323
anshusree <84310164+anshusree@users.noreply.github.com>
2023-02-01 10:15:50 +0530

    merge two binary trees

--- 25-01-2023/mergetrees.java
+++ 25-01-2023/mergetrees.java
@@ -1,10 +1,11 @@
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null && root2==null) return null;
         if(root1==null) return root2;
         if(root2==null) return root1;
-        TreeNode head = new TreeNode(root1.val + root2.val);
-        head.left = mergeTrees(root1.left, root2.left);
-        head.right = mergeTrees(root1.right, root2.right);
-        return head;
+        TreeNode node = new TreeNode(root1.val + root2.val);
+        node.left = mergeTrees(root1.left, root2.left);
+        node.right = mergeTrees(root1.right, root2.right);
+        return node;
     }
 }
