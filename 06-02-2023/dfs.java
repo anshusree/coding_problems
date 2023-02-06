ce313c31b0fa28474040c3b185ac2b628be29b40
anshusree <84310164+anshusree@users.noreply.github.com>
2023-02-07 01:03:41 +0530

    dfs traversal of graph

--- 06-02-2023/dfs.java
+++ 06-02-2023/dfs.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public void dfs(int source, boolean[] visited, ArrayList<Integer> res, ArrayList<ArrayList<Integer>> adj){
+        visited[source] = true;
+        res.add(source);
+        for(int neigh:adj.get(source)){
+            if(!visited[neigh]){
+                dfs(neigh,visited,res,adj);
+            }
+        }
+    }
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        ArrayList<Integer> res = new ArrayList<>();
+        dfs(0,visited,res,adj);
+        return res;
+    }
+}
